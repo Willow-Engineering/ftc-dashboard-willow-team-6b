1c0e578ee484f6eed5cc9db37291dbebc2534713
SalBalsamo <Sal_Balsamo.stu@willowschoolnola.org>
2023-08-31 13:24:38 -0500

    Added FtcDashboard code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CIM_HelloWorld_SB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CIM_HelloWorld_SB.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,6 +9,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class CIM_HelloWorld_SB extends OpMode {
     @Override
     public void init() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
         telemetry.addData("Hello","World");
         // sends the caption and value to the driver station
     }
