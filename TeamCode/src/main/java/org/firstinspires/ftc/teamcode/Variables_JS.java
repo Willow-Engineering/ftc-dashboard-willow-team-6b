f31a107a6f41c5a91e8a42a4b93b1c62ac7b297e
JasperSchwilke <jasper_schwilke.stu@willowschoolnola.org>
2023-09-08 14:22:04 -0500

    Create Variables_JS.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_JS.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp
+public class Variables_JS {
+
+
+}
