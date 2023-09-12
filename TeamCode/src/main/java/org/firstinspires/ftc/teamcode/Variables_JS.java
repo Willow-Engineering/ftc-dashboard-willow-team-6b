05e3e6012ef3672850461530bef92db56ba49865
JasperSchwilke <jasper_schwilke.stu@willowschoolnola.org>
2023-09-12 12:25:27 -0500

    Update Variables_JS.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_JS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_JS.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -7,6 +9,10 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @TeleOp
 public class Variables_JS {
-
-
+    int Team = 19824;
+    boolean running = false;
+    char TeamMember = J;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+    
 }
