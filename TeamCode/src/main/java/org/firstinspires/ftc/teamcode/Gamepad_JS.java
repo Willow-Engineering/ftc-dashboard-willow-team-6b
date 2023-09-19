143cd5f8748d0ef3cc8b07220b7942b6b01b7528
JasperSchwilke <jasper_schwilke.stu@willowschoolnola.org>
2023-09-19 13:50:43 -0500

    yeet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_JS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_JS.java
@@ -6,14 +6,17 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class Gamepad_JS extends OpMode {
+    private DcMotor Motor1;
 
     @Override
     public void init() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
     }
 
     @Override
@@ -22,6 +25,7 @@ public class Gamepad_JS extends OpMode {
         double LeftY = gamepad1.left_stick_y / 2;
         boolean A = gamepad1.a;
         boolean B = gamepad1.b;
+        boolean X = gamepad1.x;
         if(A = true){
             LeftX *= 2;
             LeftY *= 2;
@@ -30,8 +34,12 @@ public class Gamepad_JS extends OpMode {
             LeftX = gamepad1.left_stick_y / 2;
             LeftY = gamepad1.left_stick_x / 2;
         }
+        if(X){
+            Motor1.setPower(1);
+        }
         telemetry.addData("Left X", LeftX);
         telemetry.addData("Left Y", LeftY);
         telemetry.update();
     }
 }
+
