39ad1e0061df45c0736a3a007f3fabb76bd8521a
Cypress-Magnolia <cypress_hodgson.stu@willowschoolnola.org>
2023-11-12 00:29:38 -0600

    Update BasicLinearOpMode_CMH.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode_CMH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode_CMH.java
@@ -30,53 +30,51 @@ public class BasicLinearOpMode_CMH extends LinearOpMode {
         leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
         arm = hardwareMap.get(DcMotorEx.class, "arm");
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
         arm.setDirection(DcMotorEx.Direction.FORWARD);
         leftIntake = hardwareMap.get(Servo.class, "leftIntake");
         rightIntake = hardwareMap.get(Servo.class, "rightIntake");
         leftIntake.setDirection(Servo.Direction.FORWARD);
         rightIntake.setDirection(Servo.Direction.REVERSE);
         touch = hardwareMap.get(DigitalChannel.class, "touch");
-        int driveMode = 0;
         waitForStart();
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         runtime.reset();
+        int armPos = -50;
+        boolean dpad_upOld = false;
+        boolean dpad_downOld = false;
         while (opModeIsActive()) {
-            if (driveMode == 0) {
-                double drive = -gamepad1.left_stick_y;
-                double turn = gamepad1.left_stick_x;
-                double leftPower = Range.clip(drive + turn, -1.0, 1.0);
-                double rightPower = Range.clip(drive - turn, -1.0, 1.0);
-                //double armPower = gamepad1.right_stick_y;
-                double leftIntakePos = -.1 * gamepad1.right_trigger + .5;
-                double rightIntakePos = -.1 * gamepad1.right_trigger + .5;
-                leftDrive.setPower(leftPower);
-                rightDrive.setPower(rightPower);
-                if(gamepad1.a) {
-                    arm.setTargetPosition(-1500);
-                    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    arm.setVelocity(300);
-                }
-                else if(gamepad1.b) {
-                    arm.setTargetPosition(-750);
-                    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    arm.setVelocity(300);
-                }
-                else if (gamepad1.x && arm.getCurrentPosition() > -1600) {
-                    arm.setPower(-300);
-                }
-                else {
-                    arm.setVelocity(0);
-                }
-                
-                leftIntake.setPosition(leftIntakePos);
-                rightIntake.setPosition(rightIntakePos);
-                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-                telemetry.addData("Intake", "left (%.2f), right (%.2f)", leftIntake.getPosition(), rightIntake.getPosition());
-            }
+            int gamepad1L;
+            if (gamepad1.left_bumper) gamepad1L = 1;
+            else gamepad1L = 0;
+            /*int gamepad1R;
+            if (gamepad1.right_bumper) gamepad1R = 1;
+            else gamepad1R = 0;*/
+            double drive = (1 - .5 * gamepad1L) * gamepad1.left_stick_y;
+            double turn = (1 - .5 * gamepad1L) * .75 * gamepad1.left_stick_x;
+            double leftPower = Range.clip(drive - turn, -1.0, 1.0);
+            double rightPower = Range.clip(drive + turn, -1.0, 1.0);
+            double IntakePos = -.15 * gamepad1.right_trigger + .5;
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+            if (gamepad1.y) armPos = -1500;
+            else if (gamepad1.b) armPos = -750;
+            else if (gamepad1.a) armPos = -50;
+            else if (gamepad1.dpad_up && !dpad_upOld) armPos -= 50;
+            else if (gamepad1.dpad_down && !dpad_downOld) armPos += 50;
+            if (gamepad1.dpad_up) dpad_upOld = true;
+            if (gamepad1.dpad_down) dpad_downOld = true;
+            arm.setTargetPosition(armPos);
+            arm.setVelocity(300);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftIntake.setPosition(IntakePos);
+            rightIntake.setPosition(IntakePos);
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Intake", "left (%.2f), right (%.2f)", leftIntake.getPosition(), rightIntake.getPosition());
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Arm Position,", arm.getCurrentPosition());
+            telemetry.addData("Arm Position: ", arm.getCurrentPosition());
+            telemetry.addData("Arm Target Position: ", armPos);
             telemetry.addData("Triggers", "left (%.2f), right (%.2f)", gamepad1.left_trigger, gamepad1.right_trigger);
             telemetry.update();
         }
