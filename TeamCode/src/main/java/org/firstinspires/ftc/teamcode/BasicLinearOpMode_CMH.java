5082a505e4189eaf98e2c544d181836ac85e95e3
Cypress-Magnolia <cypress_hodgson.stu@willowschoolnola.org>
2023-11-14 13:49:13 -0600

    a little something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode_CMH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode_CMH.java
@@ -21,6 +21,14 @@ public class BasicLinearOpMode_CMH extends LinearOpMode {
     private DigitalChannel touch = null;
     Servo leftIntake;
     Servo rightIntake;
+
+    int bool2int(boolean n) {
+        int x;
+        if (n) x = 1;
+        else x = 0;
+        return x;
+    }
+
     @Override
     public void runOpMode() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
@@ -41,39 +49,44 @@ public class BasicLinearOpMode_CMH extends LinearOpMode {
         waitForStart();
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         runtime.reset();
-        int armPos = -50;
+        int armPos = 50;
         boolean dpad_upOld = false;
         boolean dpad_downOld = false;
         while (opModeIsActive()) {
-            int gamepad1L;
-            if (gamepad1.left_bumper) gamepad1L = 1;
-            else gamepad1L = 0;
-            /*int gamepad1R;
-            if (gamepad1.right_bumper) gamepad1R = 1;
-            else gamepad1R = 0;*/
-            double drive = (1 - .5 * gamepad1L) * gamepad1.left_stick_y;
-            double turn = (1 - .5 * gamepad1L) * .75 * gamepad1.left_stick_x;
+            //drive code
+            double LB = bool2int(gamepad1.left_bumper);
+            double drive = (1 - .5 * LB) * gamepad1.left_stick_y;
+            double turn = (1 - .5 * LB) * .75 * gamepad1.left_stick_x;
             double leftPower = Range.clip(drive - turn, -1.0, 1.0);
             double rightPower = Range.clip(drive + turn, -1.0, 1.0);
-            double IntakePos = -.15 * gamepad1.right_trigger + .5;
+
+            //arm code
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
-            if (gamepad1.y) armPos = -1500;
-            else if (gamepad1.b) armPos = -750;
-            else if (gamepad1.a) armPos = -50;
-            else if (gamepad1.dpad_up && !dpad_upOld) armPos -= 50;
-            else if (gamepad1.dpad_down && !dpad_downOld) armPos += 50;
-            if (gamepad1.dpad_up) dpad_upOld = true;
-            if (gamepad1.dpad_down) dpad_downOld = true;
-            arm.setTargetPosition(armPos);
-            arm.setVelocity(300);
+            float RB = bool2int(gamepad1.right_bumper);
+            if (!gamepad1.left_bumper && !gamepad1.x && !(armPos % 50 == 0)) armPos += 50 - (armPos % 50);
+            else if (gamepad1.y) armPos = 1500;
+            else if (gamepad1.b) armPos = 750;
+            else if (gamepad1.a) armPos = 50;
+            else if (gamepad1.x) armPos = 15;
+            else if (gamepad1.dpad_up && (!dpad_upOld || RB == 1) && armPos < 1500) armPos += 10 + 40 * RB;
+            else if (gamepad1.dpad_down && (!dpad_downOld || RB == 1) && armPos > 50) armPos -= 10 + 40 * RB;
+            dpad_upOld = gamepad1.dpad_up;
+            dpad_downOld = gamepad1.dpad_down;
+            arm.setTargetPosition(-armPos);
+            arm.setVelocity(500);
             arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            //intake code
+            double IntakePos = -.15 * gamepad1.right_trigger + .5;
             leftIntake.setPosition(IntakePos);
             rightIntake.setPosition(IntakePos);
+
+            //telemetry
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("Intake", "left (%.2f), right (%.2f)", leftIntake.getPosition(), rightIntake.getPosition());
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Arm Position: ", arm.getCurrentPosition());
+            telemetry.addData("Arm Position: ", -arm.getCurrentPosition());
             telemetry.addData("Arm Target Position: ", armPos);
             telemetry.addData("Triggers", "left (%.2f), right (%.2f)", gamepad1.left_trigger, gamepad1.right_trigger);
             telemetry.update();
